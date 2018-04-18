1c67d63afe5e5b076646600d8bed855ce6bfcd3a
btran24 <btran.csc@gmail.com>
2018-04-19 01:50:20 +0700

    create web api

---
+++ MentosAPI/Mentos/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Mentos.Controllers
+{
+    public class UserController : ApiController
+    {
+		[Route("api/user/products")]
+		public string GetAllProducts()
+		{
+			return "Good";
+		}
+	}
+}
---
+++ MentosAPI/Mentos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mentos.Startup))]
+namespace Mentos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
